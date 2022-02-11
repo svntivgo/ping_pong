85df1774f3efdb00cbd95fd2f9c57f69d3808d5b
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-10 20:13:42 -0500

    Movimiento barras PT2

--- main.js
+++ main.js
@@ -27,14 +27,18 @@
         this.board = board
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
 
     self.Bar.prototype = {
         down: function () {
-
+            this.y += this.speed;
         },
         up: function () {
-
+            this.y -= this.speed;
+        },
+        toString: function() {
+            return "x: "+ this.x +" y:"+ this.y;
         }
     }
 })();
@@ -70,14 +74,28 @@
     }
 })();
 
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 40, 100, board);
+var bar2 = new Bar(700, 100, 40, 100, board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas, board);
+
+document.addEventListener('keydown', function(ev) {
+    console.log(ev.key);
+    if (ev.key == 'ArrowUp') {
+        bar.up();
+    } else if (ev.key == 'ArrowDown') {
+        bar.down();
+    } else if (ev.key == 'w') {
+        bar2.up();
+    } else if (ev.key == 's') {
+        bar2.down();
+    }
+})
 window.addEventListener("load", main());
 
 function main() {
-    var board = new Board(800, 400);
-    var bar = new Bar(20, 100, 40, 100, board);
-    var bar2 = new Bar(700, 100, 40, 100, board);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas, board);
+
     console.log(board);
     board_view.draw();
 }
