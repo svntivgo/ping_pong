dd63e9e2369115626c32bd248c14c9cb282438fd
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-10 20:55:22 -0500

    creacion pelota PT4

--- main.js
+++ main.js
@@ -11,11 +11,25 @@
     self.Board.prototype = {
         get elements() {
             var elements = this.bars;
-            //elements.push(this.ball);
+            // elements.push(this.ball);
             return elements;
         }
     }
 
+})();
+
+(function() {
+    self.Ball = function(x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
 })();
 
 (function () {
@@ -37,8 +51,8 @@
         up: function () {
             this.y -= this.speed;
         },
-        toString: function() {
-            return "x: "+ this.x +" y:"+ this.y;
+        toString: function () {
+            return "x: " + this.x + " y:" + this.y;
         }
     }
 })();
@@ -54,7 +68,7 @@
 
 
     self.BoardView.prototype = {
-        clean: function() {
+        clean: function () {
             console.log(":p");
             this.ctx.clearRect(0, 0, this.board.width, this.board.heigth)
         },
@@ -64,6 +78,10 @@
 
                 draw(this.ctx, el)
             }
+        },
+        play: function () {
+            this.clean();
+            this.draw();
         }
     }
 
@@ -71,6 +89,12 @@
         switch (element.kind) {
             case "rectangle":
                 ctx.fillRect(element.x, element.y, element.width, element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
                 break;
         }
     }
@@ -81,8 +105,9 @@ var bar = new Bar(20, 100, 40, 100, board);
 var bar2 = new Bar(700, 100, 40, 100, board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
+// var ball = new Ball(350, 100, 10, board);
 
-document.addEventListener('keydown', function(ev) {
+document.addEventListener('keydown', function (ev) {
     ev.preventDefault();
     if (ev.key == 'ArrowUp') {
         bar2.up();
@@ -98,7 +123,8 @@ document.addEventListener('keydown', function(ev) {
 window.requestAnimationFrame(controller);
 
 function controller() {
-    board_view.clean();
-    board_view.draw();
+    board_view.play();
+    // board_view.clean();
+    // board_view.draw();
     window.requestAnimationFrame(controller);
 }
